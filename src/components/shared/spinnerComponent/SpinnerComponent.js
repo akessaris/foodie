bd9a3442f62679ab4b9b8e7d4416c7533a26f63f
Alexander Kessaris <ank352@nyu.edu>
2020-12-19 10:17:21 -0700

    added spinner component

---
+++ src/components/shared/spinnerComponent/SpinnerComponent.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import classes from './SpinnerComponent.module.css';
+
+const spinnerComponent = () => <div className={classes.Loader}>Loading...</div>;
+
+export default spinnerComponent;
